f844b5535346d9728dfc31f791545688d5f9c669
Silas John <paradox687A@gmail.com>
2021-02-24 23:49:49 +0000

    Finished high level organization for all tasks, need to complete finer details and descriptions now

--- P1_main.py
+++ P1_main.py
@@ -2,6 +2,7 @@
 class Manipulation:
   def __init__(self):
     self.title = "Submersible System Data Transmission and Manipulation"
+    self.reminder = "Remember to edit the values in the following dictionaries when you are updating the system values after decoding the encrypted transmissions! You may add or remove functions, parameters and arguments for methods, or organization as you would like to. Do NOT edit anything within this __init__ method."
     self.author, self.designer = 'Silas John', 'Alden Wang'
     self.systemInformationAnalog = {
       "propellors": [0, 99],
@@ -23,5 +24,31 @@ class Manipulation:
       "hitOrMiss": 'MIS',
       "videoTransmit": 1
     }
-
   
+  def outputSummary(self):
+    # insert code to form your first and last output summary of the system here
+    #
+    # you should have the same code forming an output summary before and after 
+    # system changes
+    #
+    # String Formatting is really useful in situations like this
+    pass
+
+  def openFile(self):
+    pass
+    # insert code to open and read file contents
+
+  def outputUPDATES(self):
+    # insert code for your outputting updates to the system
+    pass
+  
+  def decodeEncryption(self):
+    # insert code to decode the transmission and update the system variables in the
+    # dictionaries found above
+    # Maybe every time you decode a transmission, you can call outputUPDATES() here to instantly announce the update to the system?
+    pass
+
+  def masterManipulator(self):
+    self.outputSummary()
+    self.decodeEncryption()
+    self.outputSummary()
--- main.py
+++ main.py
@@ -1,28 +1,37 @@
 from P3_input import Quadratic
 from P3_unittest_code import TestStringMethods
+from P1_main import Manipulation
+
 import unittest
 
 class Advent:
+
   def __init__(self):
     self.introductionMessage = "Hello Prospective Advent Industries Programming Interns! Attached in this same folder/directory are additional files that should help you with your programming tasks!"
+
   def softwareTaskOne(self):
     # view P1_input.txt 
     # view P1_output.txt
+    Manipulation.masterManipulator()
     pass
+
   def softwareTaskTwo(self):
     # Use Online Research!  
     # Modify Online solutions, openCV cannot be used in repl.it
     # Write a seperate python file to solve this problem
     pass
+
   def softwareTaskThree(self):
     unittest.main()
+
   def __str__(self):
     return self.introductionMessage
+
 if __name__ == "__main__":
   instance = Advent()
   print(instance, "\n")
 
-  # start editing code here 
+  # start editing code below here 
   # Comment out the task you are not testing
   instance.softwareTaskOne()
   instance.softwareTaskThree()
