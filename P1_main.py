ac1e60959ba861bd8133a7f4af7b7461d55971c5
Silas John <paradox687A@gmail.com>
2021-02-24 23:29:29 +0000

    Worked on unit test challenge and class manipulation

--- P1_main.py
+++ P1_main.py
@@ -1,6 +1,12 @@
 
 class Manipulation:
   def __init__(self):
-    self.title = "Submersible System"
+    self.title = "Submersible System Data Transmission and Manipulation"
     self.author, self.designer = 'Silas John', 'Alden Wang'
+    self.systemInformation = {
+      "brand": "Ford",
+      "model": "Mustang",
+      "year": 1964
+    }
+
   
