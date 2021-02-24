b8758feb11d3a8eb736fb01d4fdbec53d9bd85e8
Silas John <paradox687A@gmail.com>
2021-02-24 21:05:05 +0000

    added descriptions to output for software task one

--- main.py
+++ main.py
@@ -5,12 +5,21 @@ import unittest
 class Advent:
   def __init__(self):
     self.introductionMessage = "Hello Prospective Advent Industries Programming Interns! Attached in this same folder/directory are additional files that should help you with your programming tasks!"
+  def softwareTaskOne(self):
+    # view P1_input.txt 
+    # view P1_output.txt
+    pass
+  def softwareTaskTwo(self):
+    # Use Online Research!  
+    # Modify Online solutions, openCV cannot be used in repl.it
+    pass
+  def softwareTaskThree(self):
+    # functionTestInstance = TestStringMethods()
+    unittest.main()
   def __str__(self):
     return self.introductionMessage
 if __name__ == "__main__":
-  firstInstanceObject = Advent()
-  print(firstInstanceObject, "\n")
-
-  functionInstance = TestStringMethods()
-  functionInstance.test_case_one()
-  unittest.main()
+  instance = Advent()
+  print(instance, "\n")
+  
+  instance.softwareTaskThree()
